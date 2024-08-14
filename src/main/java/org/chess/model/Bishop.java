231af6ff5ec63ec89d08822ea15a7fb735f6760e
derlansilva <joderlan.silva@gmail.com>
2024-08-14 20:02:05 -0300

    added the knight´s move logic

--- src/main/java/org/chess/model/Bishop.java
+++ src/main/java/org/chess/model/Bishop.java
@@ -7,6 +7,12 @@ public class Bishop extends Piece {
 
     @Override
     public boolean move(int startX, int startY, int endX, int endY, Board board) {
+        int dx = Math.abs(endX - startX);
+        int dy = Math.abs(endY - startY);
+        System.out.println("bispo __________________________");
+        System.out.println("DX " +dx);
+        System.out.println("DY " +dy);
+        System.out.println("bispo __________________________");
         return false;
     }
 }
--- src/main/java/org/chess/model/Knight.java
+++ src/main/java/org/chess/model/Knight.java
@@ -6,6 +6,16 @@ public class Knight extends Piece{
     }
     @Override
     public boolean move(int startX, int startY, int endX, int endY, Board board) {
+        int dX = Math.abs(endX - startX);
+        int dY = Math.abs(endY - startY);
+
+
+        System.out.println( "dX " + Math.abs(endX - startX));
+        System.out.println("dY "+ Math.abs(endY - startY));
+        if((dX ==2 && dY == 1) || (dX == 1 && dY ==2)){
+            Piece destinationPiece = board.getPiece(endX , endY);
+            return destinationPiece == null || destinationPiece.isWhite != this.isWhite;
+        }
         return false;
     }
 }
