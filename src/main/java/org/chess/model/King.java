47b9b3d3f79be485324abd59ddb5dfbc9181022a
derlansilva <joderlan.silva@gmail.com>
2024-08-15 23:43:19 -0300

    Implement king movement logic

--- src/main/java/org/chess/model/King.java
+++ src/main/java/org/chess/model/King.java
@@ -5,6 +5,16 @@ public class King extends Piece{
     public King(boolean isWhite) { super(isWhite); }
     @Override
     public boolean move(int startX, int startY, int endX, int endY, Board board) {
+        int dx = Math.abs(endX - startX);
+        int dy = Math.abs(endY - startY);
+
+        if(dx <= 1&& dy<=1){
+            Piece destinationPiece = board.getPiece(endX, endY);
+            if(destinationPiece != null && destinationPiece.isWhite()== isWhite){
+                return  false;
+            }
+            return true;
+        }
         return false;
     }
 }
